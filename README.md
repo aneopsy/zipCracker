0624002405520c8f8f41f76cec80e14d5293679e
AneoPsy <paultevatheis@gmail.com>
2016-09-16 13:28:47 -1000

    nouveau fichier : README.md  	nouveau fichier : test.txt  	nouveau fichier : test.zip  	nouveau fichier : word_list  	nouveau fichier : zipcracker.py

---
+++ README.md
@@ -0,0 +1,59 @@
+# ZipCracker -  Crack a Zip file by BruteForce.
+
+ZipCracker est un outil en Python de crack qui utilise le bruteforce avec un dictionnaire.
+
+### Prerequisities
+
+* Python2.7
+* zipfile
+```
+pip install zipfile
+```
+
+## Getting Started
+
+```
+$ python zipcracker.py
+
+usage: zipcracker.py [-h] -f FILE -w WORD_LIST [-V]
+```
+
+Creation du fichier zip chiffré:
+
+```
+$ nano test.txt
+  Voici un petit test.
+
+$ zip --encrypt test.zip test.txt
+  Enter password:
+  Verify password:
+     adding: test.txt (stored 0%)
+$ ls
+  test.txt  test.zip  zipcracker.py
+```
+
+Lancement du crack:
+
+```
+$ python zipcracker.py -f test.zip -w word_list
+(2994 / 3107) |  96.00%
+
+Password cracked: nirvana
+
+Took 0.562439 seconds to crack the password. That is, 5325 attempts per second.
+```
+
+## Deployment
+
+Crysher est compatible sur:
+
+- Linux
+
+## Authors
+
+* **AneoPsy** - *Initial work*
+
+## Acknowledgments
+
+* Cryptographie
+* Python
---
+++ zipcracker.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+
+import argparse
+import zipfile
+import os
+import sys
+from time import time
+
+VERSION = '1.0'
+AUTHOR = "AneoPsy"
+
+
+def _cli_opts():
+    '''
+    Parse command line options.
+    @returns the arguments
+    '''
+    mepath = unicode(os.path.abspath(sys.argv[0]))
+    mebase = '%s' % (os.path.basename(mepath))
+    description = '''Zip cracker.'''
+    desc = argparse.RawDescriptionHelpFormatter
+    parser = argparse.ArgumentParser(prog=mebase,
+                                     description=description,
+                                     formatter_class=desc,
+                                     )
+
+    parser.add_argument('-f', '--file',
+                        action='store',
+                        help='zip file',
+                        required=True)
+    parser.add_argument('-w', '--word_list',
+                        action='store',
+                        help='word_list file',
+                        required=True)
+    parser.add_argument('-V', '--version',
+                        action='version',
+                        version='%(prog)s v' + VERSION + " by " + AUTHOR)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main(args):
+
+    try:
+        zip_ = zipfile.ZipFile(args.file)
+    except zipfile.BadZipfile:
+        print "Please check the file's path. It doesn't seem to be a zip file."
+        sys.exit(1)
+
+    password = None
+    i = 0
+    c_t = time()
+
+    try:
+        max_lines = sum(1 for line in open(args.word_list, 'r'))
+    except Exception:
+        print "Error: wordlist not found!"
+        sys.exit(1)
+    with open(args.word_list, "r") as f:
+        passes = f.readlines()
+        for x in passes:
+            i += 1
+            password = x.split("\n")[0]
+            try:
+                zip_.extractall(pwd=password)
+                t_t = time() - c_t
+                print "\n\nPassword cracked: %s\n" % password
+                print "Took %f seconds to crack the password. That is, "\
+                      "%i attempts per second." % (t_t, i / t_t)
+                sys.exit(1)
+            except Exception:
+                pass
+            output = "(%*d / %d) | %6.2f%%" % (len(str(max_lines)),
+                                               i,
+                                               max_lines,
+                                               100 * (i + 1) / max_lines
+                                               )
+            sys.stdout.write('\b' * 80)
+            sys.stdout.write(output)
+            sys.stdout.flush()
+
+if __name__ == '__main__':
+    args = _cli_opts()
+    try:
+        main(args)
+    except KeyboardInterrupt:
+        print "\nExit..."
+        sys.exit(1)
